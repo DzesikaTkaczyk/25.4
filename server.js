9ca64aff983e29c24400dd80920b7ec52337c2fa
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-10-22 15:35:17 +0200

    add static file

--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+
 app.use(express.static('assets'));
 
+app.get('/', function (req, res) {
+	res.send('Hello world');
+});
+
+app.get('/image', function (req, res) {
+	res.sendFile('assets/ronsie.jpg' , { root : __dirname});
+});
+
 app.get('/userform', function (req, res) {
-    res.sendFile('/index.html');
-    const response = {
-        first_name: req.query.first_name,
-        last_name: req.query.last_name
-    };
-    res.json(response);
+	res.sendFile('/index.html')
+	const response = {
+		first_name: req.query.first_name,
+		last_name: req.query.last_name
+	};
+	res.json(response);
 });
 
-var server = app.listen(3000, 'localhost', function() {
-    var host = server.address().address;
-    var port = server.address().port;
+const server = app.listen(3000, 'localhost', function() {
+	const host = server.address().address;
+	const port = server.address().port;
 
-    console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
+	console.log('Przykładowa aplikacja nasłuchuje na http://' + host + ':' + port);
 });
-
-
